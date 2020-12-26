b181210f3cc623dabcc4aee9d8ad6cbea0123e8d
Daniele Borra <daniele.borra.93@gmail.com>
2020-12-26 18:38:20 +0100

    🤘🏼 First working interation

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs').promises;
+const parse = require('node-html-parser').parse;
+
+const injectStyle = (template, css) => {
+  return `\n<style>${css}</style>\n${template}`;
+}
+
+module.exports = function (snowpackConfig, pluginOptions) {
+  return {
+    name: 'dot-loader',
+    resolve: {
+      input: ['.dotc'],
+      output: ['.js'],
+    },
+    async load({ filePath }) {
+      const code = await fs.readFile(filePath);
+      const dom = parse(code);
+      const script = dom.querySelector('script');
+      const style = dom.querySelector('style');
+      const template = dom.querySelector('template');
+      if (!script) throw new Error('Component must specify at least a script tag');
+      if (!template) throw new Error('Component must specify at least a template tag');
+
+      const injectedTemplate = injectStyle(template.innerHTML, style.innerText);
+    
+      const result = `import { register, html } from 'dot';\n
+const template = () => html\`${injectedTemplate}\`;\n
+${script.innerText}
+
+register(${script.getAttribute('data-name')});
+
+export default ${script.getAttribute('data-name')};
+      `;
+      return result;
+    },
+  };
+};
