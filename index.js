e7c2ece57bf5f95dec86f11b798bf461cd428a84
Daniele Borra <daniele@adoratorio.com>
2020-12-29 10:36:05 +0100

    Added sass compiling option for style

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const fs = require('fs').promises;
 const parse = require('node-html-parser').parse;
+const sass = require('sass');
 
 const injectStyle = (template, css) => {
   return `\n<style>${css}</style>\n${template}`;
 }
 
 const t = `import { register, html } from 'dot';\n
-{{ script }}\n
-register({{ name }});\n
-export default {{ name }};`;
+{{ dot:script }}\n
+register({{ dot:name }});\n
+export default {{ dot:name }};`;
 
 module.exports = function (snowpackConfig, pluginOptions) {
   return {
@@ -28,11 +29,21 @@ module.exports = function (snowpackConfig, pluginOptions) {
       if (!script) throw new Error('Component must specify at least a script tag');
       if (!template) throw new Error('Component must specify at least a template tag');
 
-      const injectedTemplate = injectStyle(template.innerHTML, style.innerText);    
+      // Compile the style if necessary
+      let compiledStyle;
+      if (style.getAttribute('lang') === 'sass') {
+        const sassCode = style.innerText;
+        compiledStyle = sass.renderSync({ data: sassCode });
+      } else {
+        compiledStyle = style.innerText;
+      }
+
+      // Inject style in the template and complete the template
+      const injectedTemplate = injectStyle(template.innerHTML, compiledStyle);    
       let result = t
-        .replace(/{{ script }}/gi, script.innerText)
-        .replace(/{{ name }}/gi, script.getAttribute('name'))
-        .replace('// dot:inject', `this.$template = () => html\`${injectedTemplate}\``);
+        .replace(/{{ dot:script }}/gi, script.innerText)
+        .replace(/{{ dot:name }}/gi, script.getAttribute('name'))
+        .replace('// dot:template', `this.$template = () => html\`${injectedTemplate}\``);
       return result;
     },
   };
